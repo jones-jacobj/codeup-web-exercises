65c600b889b74810071d9e2ec5a4d8e10f76d3df
Jacob Jones <jacob.j.jones01@gmail.com>
2022-09-08 09:14:32 -0500

    Completed basic Javascript exercises

---
+++ js/external.js
@@ -0,0 +1,4 @@
+console.log("Hello from External Javascript")
+alert("Welcome to my Website!")
+favoriteColor = prompt("What is your favorite color?")
+alert("Great, " + favoriteColor + " is my favorite color too!")
