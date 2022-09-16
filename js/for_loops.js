f8fb2f959ef73b4d665dd0dfec82dd2b8b754ca0
Jacob Jones <jacob.j.jones01@gmail.com>
2022-09-16 09:27:07 -0500

    Finishing random & even/ odd

---
+++ js/for_loops.js
@@ -0,0 +1,18 @@
+
+function showMultiplicationTable(number){
+    let i = 1 ;
+    while(i<11){
+        console.log(Number(number) + " x " + i + " = " + (number*i));
+        i++;
+    }
+}
+/*
+    Generates randoms between 2-200 and returns if those numbers are odd or even
+ */
+function randomType(){
+    for (let i=0; i<10; i++){
+        let random = Math.floor((Math.random() * 200) + 20);
+        let typing = (random %2 !== 0) ? "odd" : "even";
+        console.log(random + " = " + typing);
+    }
+}
