94afbb525f87fba7e89a5d2c157d42b09a460999
Jacob Jones <jacob.j.jones01@gmail.com>
2022-10-25 15:14:58 -0500

    Working on weather project

---
+++ js/weather_map.js
@@ -0,0 +1,7 @@
+mapboxgl.accessToken = MAPBOX_KEY
+    var map = new mapboxgl.Map({
+        container: 'map',
+        style: 'mapbox://styles/mapbox/streets-v9',
+        zoom: 10,
+        center: [-98.4916, 29.4252]
+    });
