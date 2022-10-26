81a5c3bcf2e253796d10aa8b020631e5862d2563
Jacob Jones <jacob.j.jones01@gmail.com>
2022-10-26 14:43:35 -0500

    Working on weatherapp

--- js/weather_map.js
+++ js/weather_map.js
@@ -1,3 +1,9 @@
+// Location search button
+$('#locationSearchButton').on('click',function(){
+    let val = $('#locationSearchInput')[0].value;
+    $('#locationSearchInput')[0].value = "";    
+    getLatLong(val);
+});
 
 // Get Map, display map
 mapboxgl.accessToken = MAPBOX_KEY
@@ -8,36 +14,63 @@ mapboxgl.accessToken = MAPBOX_KEY
         center: [-98.4916, 29.4252]
     });
 
-// Get weather
-$.get("http://api.openweathermap.org/data/2.5/forecast", {
-    APPID: OPEN_WEATHER_APPID,
-    lat:    29.423017,
-    lon:   -98.48527,
-    units: "imperial"
-}).done(function(data){
-    updateFiveDayForecast(data);
-});
+function geocode(search) {
+    let token = MAPBOX_KEY;
+    var baseUrl = 'https://api.mapbox.com';
+    var endPoint = '/geocoding/v5/mapbox.places/';
+    return fetch(baseUrl + endPoint + encodeURIComponent(search) + '.json' + "?" + 'access_token=' + token)
+        .then(function(res) {
+            return res.json();
+        // to get all the data from the request, comment out the following three lines...
+        }).then(function(data) {
+            return data.features[0].center;
+        });
+}
+
+//GEOLOCATION FROM MAPBOX => OPENWEATHER
+function getLatLong(location){
+    let latLong = geocode(location);
+    getWeather(latLong);
+    
+}
+
+function getWeather(location){
+    $.get("http://api.openweathermap.org/data/2.5/forecast", {
+        APPID: OPEN_WEATHER_APPID,
+        lat:   location[0],
+        lon:   location[1], 
+        units: "imperial",
+    }).done(function(data){
+        updateFiveDayForecast(data);
+    });
+}
+
+getWeather([29.423017,-98.48527])
+
 
 function updateFiveDayForecast(data){
     console.log(data);
     // Set the City Name
     document.getElementById('cityName').innerHTML = `Current City: ${data.city.name}`;
     for (let i=1; i<6; i++){
-        setCard(i, data);
+        setCards(i, data);
     }
 }
 
-function setCard(card, data){
-    let cardName=`CARD0`+card;
-    [0,2,4,6,8].forEach((e)=>{
-        let date = `${(data["list"][e]["dt_txt"]).slice(0,-9)}`
-        let description = `Description: ${data["list"][e]["weather"][0]["description"]}`;
-        let humidity = `Humidity: ${data['list'][e]['main']['humidity']}`;
-        let wind = `Wind: ${data["list"][e]["wind"]["speed"]}`;
-        let pressure = `Pressure: ${data["list"][e]["main"]["pressure"]}`;
-        $(cardName).find('.card-header')[0].innerHTML = date;
-        $(cardName).find('.list-group-item:nth-of-type(1)')[0].innerHTML = description +"<br><br>"+humidity;
-        $(cardName).find('.list-group-item:nth-of-type(2)')[0].innerHTML = wind;
-        $(cardName).find('.list-group-item:nth-of-type(3)')[0].innerHTML = pressure;
-    });
+// Makes the cards reflect the appropriate information for that date
+function setCards(card, data){
+    let i = card;
+    let cardName=`#DAY0`+card;
+    let temp = `${(data["list"][i]["main"]["temp"])}°F`
+    let date = `${(data["list"][i]["dt_txt"]).slice(0,-9)}`
+    let description = `Description: ${data["list"][i]["weather"][0]["description"]}`;
+    let humidity = `Humidity: ${data['list'][i]['main']['humidity']}`;
+    let wind = `Wind: ${data["list"][i]["wind"]["speed"]}`;
+    let pressure = `Pressure: ${data["list"][i]["main"]["pressure"]}`;
+    $(cardName).find('.card-header')[0].innerHTML = date;
+    $(cardName).find('.list-group-item:nth-of-type(1)')[0].innerHTML = temp;
+    $(cardName).find('.list-group-item:nth-of-type(2)')[0].innerHTML = description +"<br><br>"+humidity;
+    $(cardName).find('.list-group-item:nth-of-type(3)')[0].innerHTML = wind;
+    $(cardName).find('.list-group-item:nth-of-type(4)')[0].innerHTML = pressure;
+    
 }
