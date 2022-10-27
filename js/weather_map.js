331ce452103d19b74044273607b3d8f97c1b11ef
Jacob Jones <jacob.j.jones01@gmail.com>
2022-10-27 10:04:12 -0500

    Got search working

--- js/weather_map.js
+++ js/weather_map.js
@@ -2,10 +2,10 @@
 $('#locationSearchButton').on('click',function(){
     let val = $('#locationSearchInput')[0].value;
     $('#locationSearchInput')[0].value = "";    
-    getLatLong(val);
+    geocode(val);
 });
 
-// Get Map, display map
+// Get Map => display map
 mapboxgl.accessToken = MAPBOX_KEY
     var map = new mapboxgl.Map({
         container: 'map',
@@ -23,15 +23,9 @@ function geocode(search) {
             return res.json();
         // to get all the data from the request, comment out the following three lines...
         }).then(function(data) {
-            return data.features[0].center;
+            let invertedLatLong = [data['features'][0]['center'][1],data['features'][0]['center'][0]]
+            getWeather(invertedLatLong);
         });
-}
-
-//GEOLOCATION FROM MAPBOX => OPENWEATHER
-function getLatLong(location){
-    let latLong = geocode(location);
-    getWeather(latLong);
-    
 }
 
 // Query Open Weather API for information
@@ -42,28 +36,29 @@ function getWeather(location){
         lon:   location[1], 
         units: "imperial",
     }).done(function(data){
-        console.log(data);
         updateFiveDayForecast(data);
     });
 }
 
+// Default weather coordinates (San Antonio)
 getWeather([29.423017,-98.48527])
+// Start at index 4, which is 12oclock noon
+var count = 4;
 
 function updateFiveDayForecast(data){
     // Set the City Name
     document.getElementById('cityName').innerHTML = `Current City: ${data.city.name}`;
+    count = 4;
     for (let i=1; i<6; i++){
         setCards(i, data);
     }
 }
-// Start at index 4, which is 12oclock noon
-var count = 4;
 // Makes the cards reflect the appropriate information for that date
 function setCards(card, data){
     // Set values
     let cardName=`#DAY0`+card;
-    let temp = `${(data["list"][count]["main"]["temp"])}°F`
-    let date = `${(data["list"][count]["dt_txt"]).slice(0,-9)}`
+    let temp = `${data["list"][count]["main"]["temp"]}°F`;
+    let date = `${data["list"][count]["dt_txt"]}`.slice(0,-9);
     let description = `Description: ${data["list"][count]["weather"][0]["description"]}`;
     let humidity = `Humidity: ${data['list'][count]['main']['humidity']}`;
     let wind = `Wind: ${data["list"][count]["wind"]["speed"]}`;
