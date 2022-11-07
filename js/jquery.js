b336b4a030d847528e7f519daecf7cf9755d6750
Jacob Jones <jacob.j.jones01@gmail.com>
2022-11-07 14:50:26 -0600

    removed javascript from inside HTML file

---
+++ js/jquery.js
@@ -0,0 +1,9 @@
+
+// window.onload = () => {
+//     alert( 'The page has finished loading!' );
+// }
+// alert($('#liItem'));
+// $(".codeup").css("border",'solid 1px red');
+$('li').css("font-size","20px");
+$('h1, p, li').css("background-color",'yellow');
+alert($('h1').html);
